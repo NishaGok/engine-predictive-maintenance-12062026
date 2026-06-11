0c495f566cb832ea6a7a87475e067c126a979bca
NishaGok <nisha_sama@yahoo.com>
2026-06-11 15:20:17 +0000

    second commit-12062026

---
+++ predictive_maintenance_project_12062026/hosting/hosting.py
@@ -0,0 +1,21 @@
+
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(
+    token=os.getenv("HF_TOKEN")
+)
+
+api.upload_folder(
+
+    folder_path=
+    "predictive_maintenance_project/deployment",
+
+    repo_id=
+    "NishaGok/engine-predictive-maintenance-app-12062026",
+
+    repo_type="space"
+
+)
+
+print("Deployment uploaded successfully")
